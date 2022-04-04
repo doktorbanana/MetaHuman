5cbfa172e76ca4e87ef1c6f0858fa5f49e006adc
Peter Pompös <pit.martin@web.de>
2022-04-04 13:22:47 +0200

    VAE update

--- SoundGenerator.py
+++ SoundGenerator.py
@@ -1,25 +1,25 @@
 import librosa
 from Variational_Autoencoder_alla_Valerio import VAE
 import numpy as np
-import pickle
+
 
 class SoundGenerator:
-    def __init__(self, modelName, subfolder, hop_length):
-        self.model = model = VAE.load(modelName)
-        self.subfolder = subfolder
+    def __init__(self, model_name, sub_folder, hop_length, n_fft, win_size):
+        self.model = VAE.load(model_name)
+        self.sub_folder = sub_folder
         self.hop_length = hop_length
+        self.n_fft = n_fft
+        self.win_size = win_size
 
     def generate_Song(self):
         song_order = self.generate_song_order()
         spectograms = self.generate_spectograms(song_order)
-        wavesets_audio = self.convert_spectos_to_audio(spectograms)
-        song_audio = self.concatenate_wavesets(wavesets_audio)
+        song_audio = self.convert_spectos_to_audio(spectograms)
         return song_audio
-
 
     def generate_song_order(self):
         ## Must be replaced
-        path = "data/" + self.subfolder + "song_orders.npy"
+        path = "data/" + self.sub_folder + "song_orders.npy"
         song_orders = np.load(path, allow_pickle=True)
         print(f"Loaded {len(song_orders)} Songs. Will choose randomly")
         song_order = song_orders[np.random.randint(0, len(song_orders))]
@@ -31,8 +31,8 @@ class SoundGenerator:
         print(f"Spectograms Shape: {spectograms.shape}")
         return spectograms
 
-    def _denormalise(self, array, original_min, original_max):
-        denorm_array = (array - array.min()) / (array.max() - array.min())
+    def _denormalise(self, array, current_min, current_max, original_min, original_max):
+        denorm_array = (array - current_min) / (current_max - current_min)
         denorm_array = denorm_array * (original_max - original_min) + original_min
         return denorm_array
 
@@ -40,19 +40,28 @@ class SoundGenerator:
         signals = []
         for specto in spectograms:
             reshaped_specto = specto[:, :, 0]
-            denorm_specto = self._denormalise(reshaped_specto, -100, 0)
+            denorm_specto = self._denormalise(reshaped_specto, 0, 1, -100, 100)
             lin_specto = librosa.db_to_amplitude(denorm_specto)
-            signal = librosa.istft(lin_specto, hop_length=self.hop_length)
-            signals.append(signal)
+            pca = librosa.istft(lin_specto, hop_length=self.hop_length, n_fft=self.n_fft, win_length=self.win_size)
+            signal = self.cut_on_zero_edge(pca)
+            signals.extend(signal)
         print(f"One Waveset has {signals[0].shape} samples")
         print(f"This song has {len(signals)} wavesets.")
         return signals
 
-    def concatenate_wavesets(self, signals):
-        amplitudes = np.asarray(signals).flatten()
-        print(f"This song has {amplitudes.shape} samples")
-        return amplitudes
+    def cut_on_zero_edge(self, pca):
+        zero_crossings = np.argwhere((np.sign(pca[:-1]) == -1) & (np.sign(pca[1:]) == 1))
+        last_zero = zero_crossings[-1][0]
+        first_zero = zero_crossings[0][0]
+        cutted_pca = pca[first_zero:last_zero]
+        return cutted_pca
+
 
 if __name__ == "__main__":
-    sound_generator = SoundGenerator("VAE_2D_0.5_64", "0.5_64", 360)
-    sound_generator.generate_Song()
+
+    WIN_LENGTH = int(44100 / 8)
+    HOP_LENGTH = int(44100 / 23)
+    N_FFT = int(44100 / 8)
+
+    sound_generator = SoundGenerator("VAE_2D_1.0_24_2500samples", "1.0_24", HOP_LENGTH, N_FFT, WIN_LENGTH)
+    sound_generator.generate_Song() 
--- Variational_Autoencoder_alla_Valerio.py
+++ Variational_Autoencoder_alla_Valerio.py
@@ -3,6 +3,7 @@ from tensorflow.keras.layers import Input, Conv2D, ReLU, BatchNormalization, Fla
     Conv2DTranspose, Activation, Lambda
 from tensorflow.keras import backend as K
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import load_model
 from tensorflow.keras.losses import MeanSquaredError
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,7 +22,7 @@ AUTOENCODER CLASS
 
 class VAE:
 
-    def __init__(self, input_shape, conv_filters, conv_kernels, conv_strides, latent_space_dim, num_of_train_data=None):
+    def __init__(self, input_shape, conv_filters, conv_kernels, conv_strides, latent_space_dim, num_of_train_data=0):
         self.input_shape = input_shape  # dimension of input data: frequency-bins, time-windows, amplitude
         self.conv_filters = conv_filters  # a list with the number of filters per layer
         self.conv_kernels = conv_kernels  # a list with the kernel size per layer
@@ -52,11 +53,11 @@ class VAE:
     """
 
     def _build_encoder(self):
-        encoder_input = Input(shape=self.input_shape, name="Encoder_Input")
+        encoder_input = Input(shape=self.input_shape, name="encoder_input")
         conv_layers = self._add_conv_layers(encoder_input)
         bottleneck = self._add_bottleneck(conv_layers)
         self._model_input = encoder_input
-        self.encoder = Model(encoder_input, bottleneck, name="Encoder")
+        self.encoder = Model(encoder_input, bottleneck, name="encoder")
 
     def _add_conv_layers(self, encoder_input):
         x = encoder_input
@@ -71,22 +72,25 @@ class VAE:
             kernel_size=self.conv_kernels[layer_index],
             strides=self.conv_strides[layer_index],
             padding="same",
-            name=f"Encoder_Conv_Layer_{layer_index + 1}")
+            name=f"encoder_conv_layer_{layer_index + 1}")
         x = conv_layer(x)  # apply the new conv to x -> Convolution with kernels give multiple 2D Arrays
-        x = ReLU(name=f"Encoder_ReLU_{layer_index + 1}")(x)  # apply a ReLU activation to x
-        x = BatchNormalization(name=f"Encoder_BN_{layer_index + 1}")(
+        x = ReLU(name=f"encoder_relu_{layer_index + 1}")(x)  # apply a ReLU activation to x
+        x = BatchNormalization(name=f"encoder_bn_{layer_index + 1}")(
             x)  # apply Batch Normalization to x
                 # (less overfitting-problems, no vanishing Gradient or exploding Gradient)
         return x
 
+
+
     def _add_bottleneck(self, x):
         self._shape_before_bottleneck = K.int_shape(x)[1:]  # Ignore the first dim, which is the batch size
-        x = Flatten(name="Encoder_Flatten")(x)  # Flatten Data
+        x = Flatten(name="encoder_flatten")(x)  # Flatten Data
 
         # Gaussian Sampling: Sample a point in the gaussian distribution from a point in standard normal distribution
-        self.mu = Dense(self.latent_space_dim, name="Min_Vector_mu")(x)
-        self.log_variance = Dense(self.latent_space_dim, name="Log_Variance")(x)
+        self.mu = Dense(self.latent_space_dim, name="min_vector_mu")(x)
+        self.log_variance = Dense(self.latent_space_dim, name="log_variance")(x)
 
+        """
         def sample_point_from_normal_dist(args):
             mu, log_variance = args
             epsilon = K.random_normal(shape=K.shape(self.latent_space_dim), mean=0., stddev=1.)
@@ -94,8 +98,11 @@ class VAE:
             return sampled_point
 
         x = Lambda(sample_point_from_normal_dist,
-                   name="Encoder_Output")([self.mu, self.log_variance])
+                   name="encoder_output")([self.mu, self.log_variance])
+        """
+        x = LambdaLayer(self.latent_space_dim)([self.mu, self.log_variance])
         return x
+
 
     """
     ------------
@@ -104,18 +111,18 @@ class VAE:
     """
 
     def _build_decoder(self):
-        decoder_input = Input(shape=self.latent_space_dim, name="Decoder_Input")
+        decoder_input = Input(shape=self.latent_space_dim, name="decoder_input")
         dense_layer = self._add_dense_layer(decoder_input)
-        reshape_layer = Reshape(self._shape_before_bottleneck, name="Decoder_Reshape_Layer")(dense_layer)
+        reshape_layer = Reshape(self._shape_before_bottleneck, name="decoder_reshape_layer")(dense_layer)
         conv_transpose_layers = self._add_conv_transpose_layers(reshape_layer)
         decoder_output = self._add_decoder_output(conv_transpose_layers)
-        self.decoder = Model(decoder_input, decoder_output, name="Decoder")
+        self.decoder = Model(decoder_input, decoder_output, name="decoder")
 
     def _add_dense_layer(self, decoder_input):
         num_neurons = np.prod(
             self._shape_before_bottleneck)  # Product of the dimensions before the latent space
         # -> Size of the flattened data before Latent Space
-        dense_layer = Dense(num_neurons, name="Decoder_Dense")(decoder_input)
+        dense_layer = Dense(num_neurons, name="decoder_dense")(decoder_input)
         return dense_layer
 
     def _add_conv_transpose_layers(self, x):
@@ -132,11 +139,11 @@ class VAE:
             kernel_size=self.conv_kernels[layer_index],
             strides=self.conv_strides[layer_index],
             padding="same",
-            name=f"Decoder_conv_transpose_layer_{self._num_conv_layers - layer_index}"
+            name=f"decoder_conv_transpose_layer_{self._num_conv_layers - layer_index}"
         )
         x = conv_transpose_layer(x)
-        x = ReLU(name=f"Decoder_ReLU_{self._num_conv_layers - layer_index}")(x)
-        x = BatchNormalization(name=f"Decoder_BN_{self._num_conv_layers - layer_index}")(x)
+        x = ReLU(name=f"decoder_relu_{self._num_conv_layers - layer_index}")(x)
+        x = BatchNormalization(name=f"decoder_bn_{self._num_conv_layers - layer_index}")(x)
         return x
 
     def _add_decoder_output(self, x):
@@ -145,10 +152,10 @@ class VAE:
             kernel_size=self.conv_kernels[0],
             strides=self.conv_strides[0],
             padding="same",
-            name=f"Decoder_conv_transpose_layer_{self._num_conv_layers}"
+            name=f"decoder_conv_transpose_layer_{self._num_conv_layers}"
         )
         x = conv_transpose_layer(x)
-        output_layer = Activation("sigmoid", name="Decoder_Output_Sigmoid")(x)
+        output_layer = Activation("sigmoid", name="decoder_output_sigmoid")(x)
         return output_layer
 
     """
@@ -160,11 +167,26 @@ class VAE:
         optimizer = Adam(learning_rate=learning_rate)
         self.model.compile(optimizer=optimizer,
                            loss=self._calculate_combined_loss,
-                           metrics=["mse", "acc"])
+                           metrics=[self.calculate_reconstruction_loss])
+
+    def _calculate_combined_loss(self, y_true, y_pred):
+        reconstruction_loss = self.calculate_reconstruction_loss(y_true, y_pred)
+        kl_loss = -0.5 * K.sum(1 + self.log_variance - K.square(self.mu) - K.exp(self.log_variance), axis=1)
+        loss = (self.reconstruction_loss_weight * reconstruction_loss) + kl_loss
+
+        return loss
+
+    @staticmethod
+    def calculate_reconstruction_loss(y_true, y_pred):
+        # Mean Squared Error
+        error = y_true - y_pred
+        reconstruction_loss = K.mean(K.square(error), axis=[1, 2, 3])  # mean squared error
+        return reconstruction_loss
+
 
 
     def train(self, x_train, batch_size, num_epochs):
-        self.num_of_train_data = x_train.shape[0]
+        self.num_of_train_data += x_train.shape[0]
         self.model.fit(x_train,
                        x_train,
                        batch_size=batch_size,
@@ -175,23 +197,7 @@ class VAE:
     def _build_autoencoder(self):
         model_input = self._model_input
         model_output = self.decoder(self.encoder(model_input))
-        self.model = Model(model_input, model_output, name="VAE")
-
-    def _calculate_combined_loss(self, y_target, y_predicted):
-        reconstruction_loss = self._calculate_reconstruction_loss(y_target, y_predicted)
-        kl_loss = self._calculate_kl_loss(y_target, y_predicted)
-        combined_loss = self.reconstruction_loss_weight * reconstruction_loss + kl_loss
-        return combined_loss
-
-    def _calculate_reconstruction_loss(self, y_true, y_pred):
-        error = y_true - y_pred
-        reconstruction_loss = K.mean(K.square(error), axis=[1, 2, 3])  # mean squared error
-        return reconstruction_loss
-
-    def _calculate_kl_loss(self, y_true, y_pred):
-        # Kullback_Leibler Divergence, the difference between our distribution and the standard normal distribution
-        kl_loss = -0.5 * K.sum(1 + self.log_variance - K.square(self.mu) - K.exp(self.log_variance), axis=1)
-        return kl_loss
+        self.model = Model(model_input, model_output, name="Valerio")
 
     def summary(self):
         self.model.summary()
@@ -208,6 +214,7 @@ class VAE:
         self._create_folder(save_folder)
         self._save_parameters(save_folder)
         self._save_weights(save_folder)
+        self.model.save(save_folder + "\\" + "model.h5") # recursion error. too many trainable parameters?
 
     def _create_folder(self, save_folder):
         if not os.path.exists(save_folder):
@@ -234,16 +241,50 @@ class VAE:
 
     @classmethod
     def load(cls, save_folder="."):
+        # Load the parameters:
         parameters_path = os.path.join(save_folder, "parameters.pkl")
         with open(parameters_path, "rb") as f:
             parameters = pickle.load(f)
-        autoencoder = VAE(*parameters)
-        weights_path = os.path.join(save_folder, "weights.h5")
-        autoencoder.model.load_weights(weights_path)
+        loaded_autoencoder = VAE(*parameters)
 
-        print(f"This model was trained with {autoencoder.num_of_train_data} wavesets "
-              f"and has a {autoencoder.latent_space_dim}D latent space")
-        return autoencoder
+        weights_path = os.path.join(save_folder, "weights.h5")
+        loaded_autoencoder.model.load_weights(weights_path)
+        """
+        #Load Model:
+        model_path = os.path.join(save_folder, "model.h5")
+        loaded_autoencoder.model = load_model(model_path, custom_objects={'LambdaLayer': LambdaLayer,
+                                                                    '_calculate_combined_loss': cls._calculate_combined_loss,
+                                                                    'calculate_reconstruction_loss': cls.calculate_reconstruction_loss}
+                                                                    )
+
+        print(f"This model was trained with {loaded_autoencoder.num_of_train_data} wavesets "
+              f"and has a {loaded_autoencoder.latent_space_dim}D latent space")
+        """
+        return loaded_autoencoder
+
+
+"""
+-------------
+Custom Layer
+-------------
+"""
+class LambdaLayer(tf.keras.layers.Layer):
+    def __init__(self, latent_space_dim):
+        super(LambdaLayer, self).__init__()
+        self.latent_space_dim = latent_space_dim
+
+    def call(self, inputs):
+        mu, log_variance = inputs
+        epsilon = K.random_normal(shape=K.shape(self.latent_space_dim), mean=0., stddev=1.)
+        sampled_point = mu + K.exp(log_variance / 2) * epsilon
+        return sampled_point
+
+    def get_config(self):
+        return {'latent_space_dim': self.latent_space_dim}
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
 
 
 if __name__ == "__main__":
@@ -253,17 +294,9 @@ if __name__ == "__main__":
     Loading the Data
     -----------------
     """
-    subfolder = "0.5_64"
-
-    def load_data():
-        spectogram_data = np.load("data\\" + subfolder + "spectos.npy")
-        song_labels = np.load("data\\" + subfolder + "song_labels.npy")
-        position_labels = np.load("data\\" + subfolder + "position_labels.npy")
-        return spectogram_data
-
-
-    x_train = load_data()
-
+    subfolder = "0.25_6"
+    x_train = np.load("data_and_models\\" + subfolder + "\\drum_spectos.npy")
+    x_train = x_train[:1000]
 
     """
     ------------------------
@@ -271,17 +304,17 @@ if __name__ == "__main__":
     ------------------------
     """
 
-
-
     autoencoder = VAE(
         input_shape=(x_train[0].shape[0], x_train[0].shape[1], x_train[0].shape[2]),
-        conv_filters=(32, 64, 64, 64),
-        conv_kernels=(3, 3, 3, 3),
-        conv_strides=(1, 2, 2, 1),
-        latent_space_dim=2
+        conv_filters=(512, 256, 128, 64, 32),
+        conv_kernels=(3, 3, 3, 3, 3),
+        conv_strides=(2, 2, 2, 2, (2,1)),
+        latent_space_dim=128
     )
 
     autoencoder.summary()
+
+    print(x_train.shape)
 
     """
     ---------------------
@@ -296,10 +329,15 @@ if __name__ == "__main__":
     autoencoder.compile_model(LEARNING_RATE)
     autoencoder.train(x_train, BATCH_SIZE, EPOCHS)
 
+
     """
     ----------------
     Save VAE
     ----------------
     """
 
-    autoencoder.save("VAE_" + str(autoencoder.latent_space_dim) + "D_" + subfolder)
+    save_path = "data_and_models\\" + subfolder + "\\VAE_" + str(autoencoder.latent_space_dim) + "D_" + str(autoencoder.num_of_train_data) + "samples_" + str(EPOCHS) + "Epochs"
+    autoencoder.save(save_path)
+    #test_load = VAE.load(save_path)
+    #test_load.summary()
+
