c50476420a2146c537336c4b40e1436c1546b073
Peter Pompös <pit.martin@web.de>
2022-04-19 09:38:03 +0200

    Update StemExtractor.py

--- StemExtractor.py
+++ StemExtractor.py
@@ -6,6 +6,9 @@ from spleeter.audio.adapter import AudioAdapter
 import glob
 import numpy as np
 import os
+import tensorflow as tf
+
+
 
 
 def _check_rms(data, path="Unkown"):
@@ -17,45 +20,56 @@ def _check_rms(data, path="Unkown"):
 
 
 if __name__ == '__main__':
+    #print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+    num = 50
+    step_size = 450
+
     separator = Separator('spleeter:4stems')
-    path = os.path.join('/Volumes/LaptopData/FynnFrei/ML_Data/YoutubeSongs2', "*")
+    path = os.path.join('E:/FynnFrei/ML_Data/YoutubeSongs', "*")
     list_of_files = glob.glob(path)
     start_time = time.time()
 
-    audio_loader = AudioAdapter.default()
-    sample_rate = 44100
+    list_of_files = list_of_files[num:num+step_size]
+
+    for i,file in enumerate(list_of_files):
+        separator.separate_to_file(file,
+                                   "E:/FynnFrei/ML_Data/stems",
+                                   filename_format="{instrument}/{instrument}" + str(i+num) + ".{codec}",
+                                   synchronous=False)
+    separator.join()
 
     # Perform the Separation
-    for i,file in enumerate(list_of_files):
-        waveform, _ = audio_loader.load(file, sample_rate=sample_rate)
-        prediction = separator.separate(waveform)
-        vocals = prediction["vocals"]
-        bass = prediction["bass"]
-        drums = prediction["drums"]
+    #for i,file in enumerate(list_of_files):
+        # waveform, _ = audio_loader.load(file, sample_rate=sample_rate)
+        # prediction = separator.separate()
+        # vocals = prediction["vocals"]
+        # bass = prediction["bass"]
+        # drums = prediction["drums"]
 
-        empty = _check_rms(vocals, file)
-        save_path = os.path.join("demo_data/stems2/vocals", "vocals" + str(i) +".wav")
-        if empty<1:
-            sf.write(file=save_path, data=vocals, samplerate=44100)
-
-        empty = _check_rms(bass, file)
-        save_path = os.path.join("demo_data/stems2/bass", "bass" + str(i) +".wav")
-        if empty<1:
-            sf.write(file=save_path, data=bass, samplerate=44100)
-
-        empty = _check_rms(drums, file)
-        save_path = os.path.join("demo_data/stems2/drums", "drums" + str(i) + ".wav")
-        if empty < 1:
-            sf.write(file=save_path, data=drums, samplerate=44100)
-
-
-
-        print(str(i+1) + " of " + str(len(list_of_files)) +" Songs done")
+        # empty = _check_rms(vocals, file)
+        # save_path = os.path.join("E:/FynnFrei/ML_Data/stems/vocals", "vocals" + str(i + (num)) +".wav")
+        # if empty<1:
+        #     sf.write(file=save_path, data=vocals, samplerate=44100)
+        #
+        # empty = _check_rms(bass, file)
+        # save_path = os.path.join("E:/FynnFrei/ML_Data/stems/bass", "bass" + str(i + num) +".wav")
+        # if empty<1:
+        #     sf.write(file=save_path, data=bass, samplerate=44100)
+        #
+        # empty = _check_rms(drums, file)
+        # save_path = os.path.join("E:/FynnFrei/ML_Data/stems/drums", "drums" + str(i + num) + ".wav")
+        # if empty < 1:
+        #     sf.write(file=save_path, data=drums, samplerate=44100)
 
 
-    end_time = time.time()
 
-    print("took me " + str(end_time-start_time) + " seconds to finish " + str(len(list_of_files)) + " songs")
+        #print(str(i+1) + " of " + str(len(list_of_files)) +" Songs done")
+
+
+    #end_time = time.time()
+
+    #print("took me " + str(end_time-start_time) + " seconds to finish " + str(len(list_of_files)) + " songs")
 
 
 
